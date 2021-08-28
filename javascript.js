7330d789bb06d3936b9b55c6c5260e8f38dada60
GiorgioAcossi <acossigiorgio@gmail.com>
2021-08-28 16:40:37 +0200

    fix

---
+++ javascript.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // let body = document.querySelector('body');
+    // body.classList.toggle('');
+
+    window.addEventListener('scroll', function() {
+        var nav = document.getElementById('navbar');
+        console.log(nav);
+        let windowPosition = window.scrollY > 0;
+        nav.classList.toggle('bg-light-dark', windowPosition);
+    })
+
+    ScrollReveal().reveal('.reveal_left', { origin: 'left', distance: '20%', duration: 1000, easing: 'cubic-bezier(.215, .61, .355, 1)', interval: 500 });
+
+    ScrollReveal().reveal('.reveal_right', { origin: 'right', distance: '20%', duration: 1000, easing: 'cubic-bezier(.215, .61, .355, 1)', interval: 500 });
+
+    ScrollReveal().reveal('.reveal_buttom', { distance: '20%', duration: 1000, easing: 'cubic-bezier(.215, .61, .355, 1)', interval: 500 });
+
+    ScrollReveal().reveal('.reveal', { duration: 2000, easing: 'cubic-bezier(.215, .61, .355, 1)', interval: 500 });
+
+
+
+    var nav = document.getElementById('navbar');
+    if (nav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#navbar',
+            offset: 75,
+        });
+    };
+
+});
